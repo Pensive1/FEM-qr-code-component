428bfb2ab4d9d050f93ae57d411eefbe8dc3efe3
Richard Acquaye <racquaye89@gmail.com>
2024-03-04 00:01:32 +0000

    feat: Style results component (mobile)

--- 1_age-calculator-app/src/components/Results/Results.tsx
+++ 1_age-calculator-app/src/components/Results/Results.tsx
@@ -1,22 +1,23 @@
 import { Result } from "../../types/ComponentProps";
+import "./Results-styles.scss";
 
 const Results = ({ resultYears, resultMonths, resultDays }: Result) => {
   return (
     <section className="age">
       <p className="age__data">
-        <span className="age__year">
+        <span className="age__value">
           {resultYears !== null ? resultYears : "--"}
         </span>{" "}
         years
       </p>
       <p className="age__data">
-        <span className="age__months">
+        <span className="age__value">
           {resultMonths !== null ? resultMonths : "--"}
         </span>{" "}
         months
       </p>
       <p className="age__data">
-        <span className="age__days">
+        <span className="age__value">
           {resultDays !== null ? resultDays : "--"}
         </span>{" "}
         days
