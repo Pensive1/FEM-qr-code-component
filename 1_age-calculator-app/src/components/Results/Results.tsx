9ee160d08551d92b0d5f51ef5e181e7ddff0aa45
Richard Acquaye <racquaye89@gmail.com>
2024-03-08 10:24:49 +0000

    feat: Create count-up animation

--- 1_age-calculator-app/src/components/Results/Results.tsx
+++ 1_age-calculator-app/src/components/Results/Results.tsx
@@ -1,12 +1,32 @@
+import { useEffect, useRef, useState } from "react";
 import { Result } from "../../types/ComponentProps";
+import countUp from "../../utils/Animations";
 import "./Results-styles.scss";
 
 const Results = ({ resultYears, resultMonths, resultDays }: Result) => {
+  const [yearCounter, setYearCounter] = useState(0);
+  const [monthCounter, setMonthCounter] = useState(0);
+  const [dayCounter, setDayCounter] = useState(0);
+
+  let yearHandler = useRef<undefined | number>();
+
+  const countYears = () => {
+    if (resultYears) {
+      yearHandler.current = countUp(resultYears, setYearCounter);
+      return () =>
+        yearCounter === resultYears && clearInterval(yearHandler.current);
+    }
+  };
+
+  useEffect(() => {
+    countYears();
+  }, [resultYears]);
+
   return (
     <section className="age">
       <p className="age__data">
         <span className="age__value">
-          {resultYears !== null ? resultYears : "--"}
+          {resultYears !== null ? yearCounter : "--"}
         </span>{" "}
         <span className="age__unit">years</span>
       </p>
---
+++ 1_age-calculator-app/src/utils/Animations.tsx
@@ -0,0 +1,48 @@
+const countUp = (
+  targetVal: number,
+  uiUpdateFn: Function
+  //   uiRef: React.MutableRefObject<undefined>
+) => {
+  //1 Fetch the existing value (from dom)
+  // 2. Start from 0
+  const steps = 3;
+  let startVal = 0;
+  const remainder = targetVal % steps;
+  const softTargetVal = targetVal - remainder;
+
+  //   reset per function call
+  uiUpdateFn(startVal);
+
+  const counter = setInterval(() => {
+    // for number of steps -> increase by steps
+    console.log(
+      `start val: ${startVal}, Soft target: ${softTargetVal}, Target: ${targetVal}`
+    );
+
+    if (startVal < softTargetVal) {
+      uiUpdateFn((startVal += steps));
+      return;
+    }
+
+    if (startVal >= softTargetVal && startVal !== targetVal) {
+      uiUpdateFn(++startVal);
+    }
+
+    if (startVal === targetVal) {
+      clearInterval(counter);
+    }
+  }, 20);
+
+  return counter;
+
+  // ----------------------
+
+  // const spd = 97;
+  // const inc = targetVal / spd;
+
+  // if (startVal < targetVal){
+
+  // }
+};
+
+export default countUp;
